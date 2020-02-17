247a5806e71bfd82960eaf31a5f2bf65b6adc10f
IegorSoboliev <evsobolev@gmail.com>
2020-02-17 16:40:25 +0200

    HW32_2: implemented using Spring

--- src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/CinemaHallDaoImpl.java
@@ -2,24 +2,29 @@ package com.dev.cinema.dao.impl;
 
 import com.dev.cinema.dao.CinemHallDao;
 import com.dev.cinema.exceptions.DataProcessingException;
-import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.CinemaHall;
-import com.dev.cinema.util.HibernateUtil;
 
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaQuery;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
 
-@Dao
+@Repository
 public class CinemaHallDaoImpl implements CinemHallDao {
+    private SessionFactory sessionFactory;
+
+    public CinemaHallDaoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public CinemaHall add(CinemaHall cinemaHall) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             Long id = (Long) session.save(cinemaHall);
             transaction.commit();
@@ -35,7 +40,7 @@ public class CinemaHallDaoImpl implements CinemHallDao {
 
     @Override
     public List<CinemaHall> getAll() throws DataProcessingException {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             CriteriaQuery<CinemaHall> criteriaQuery =
                     session.getCriteriaBuilder().createQuery(CinemaHall.class);
             criteriaQuery.from(CinemaHall.class);
