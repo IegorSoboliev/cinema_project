d90d57eba1b99a8b67daef8800750210db7e3722
IegorSoboliev <evsobolev@gmail.com>
2020-02-20 16:01:57 +0200

    deleted add method in UserController

--- src/main/java/com/dev/cinema/controllers/UserController.java
+++ src/main/java/com/dev/cinema/controllers/UserController.java
@@ -1,13 +1,10 @@
 package com.dev.cinema.controllers;
 
-import com.dev.cinema.dto.UserRequestDto;
 import com.dev.cinema.dto.UserResponseDto;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,15 +18,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user")
-    public void addUser(@RequestBody UserRequestDto userRequestDto) {
-        User user = new User();
-        user.setEmail(userRequestDto.getEmail());
-        user.setPassword(userRequestDto.getPassword());
-        userService.add(user);
-    }
-
-    @GetMapping("/email")
+    @GetMapping("/user")
     public UserResponseDto getUserByEmail(@RequestParam("email") String email) {
         User user = userService.getByEmail(email);
         UserResponseDto userResponseDto = new UserResponseDto();
